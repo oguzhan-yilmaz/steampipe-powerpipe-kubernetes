1e1ef4ddbca55d94cce4c0aa1c79cc7ed3a4e1a1
Oguzhan Yilmaz <oguzhan@hepapi.com>
2024-10-10 22:19:24 +0300

    add steampipe docker image

---
+++ steampipe/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Running entrypoint.sh script..."
+
+
+if [ -n "$INSTALL_PLUGINS" ]; then
+    echo "Installing Plugins: $INSTALL_PLUGINS"
+    ./steampipe plugin install "$INSTALL_PLUGINS" > /dev/null
+fi
+
+echo "Updating Plugins..."
+./steampipe plugin update --all > /dev/null
+
+echo "Plugin List:"
+./steampipe plugin list
+
+echo "Starting Steampipe:"
+./steampipe service start --foreground 
