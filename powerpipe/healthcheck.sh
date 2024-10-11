10c9ad5dbfba80266684e5959b1b37eced04449f
Oguzhan Yilmaz <oguzhan@hepapi.com>
2024-10-11 17:30:12 +0300

    update powerpipe healtcheck script

--- powerpipe/healthcheck.sh
+++ powerpipe/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Perform the health check by sending a HEAD request
-response=$(curl -I http://0.0.0.0:9033 2>/dev/null)
+response=$(curl -I http://steampipe:9033 2>/dev/null)
 
 # Check if the response contains "200 OK"
 if echo "$response" | grep -q "200 OK"; then
