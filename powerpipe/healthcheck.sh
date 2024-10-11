573b88ac1fdcc35c2a96b0556018cde8a728b07d
Oguzhan Yilmaz <oguzhan@hepapi.com>
2024-10-11 13:29:31 +0300

    update dockerfile + healthcheck for powerpipe

---
+++ powerpipe/healthcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Perform the health check by sending a HEAD request
+response=$(curl -I http://0.0.0.0:9033 2>/dev/null)
+
+# Check if the response contains "200 OK"
+if echo "$response" | grep -q "200 OK"; then
+    echo "Health check passed: Received 200 OK."
+    exit 0
+else
+    echo "Health check failed: Did not receive 200 OK."
+    exit 1
+fi
